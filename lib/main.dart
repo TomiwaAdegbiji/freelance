a5a1109e3417ef0e44d0ca58f8f856ae1de1e7d0
TomiwaAdegbiji <tomiwaadegbiji@gmail.com>
2021-08-31 12:19:30 +0100

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Login Page"),
+      elevation: 0,
+      backgroundColor: Colors.green,),
+    );
+  }
+}
+
